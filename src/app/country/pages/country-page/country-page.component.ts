8b32e993f7f151e6355b1654e7660fba0b0a7f48
jcdev92 <jcdeveloper92@gmail.com>
2025-06-10 14:53:34 -0400

    feat(+ component): :sparkles: add all the form component configurations

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,9 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent { }
