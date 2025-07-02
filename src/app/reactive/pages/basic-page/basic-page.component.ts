702b1bfdfb54d3c9a607b651d3da758f10b00815
jcdev92 <jcdeveloper92@gmail.com>
2025-07-01 20:08:34 -0400

    feat(+ logic): :sparkles: handle fom validaion error responses

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -10,7 +10,7 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 export class BasicPageComponent {
   fb = inject(FormBuilder);
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
@@ -22,4 +22,40 @@ export class BasicPageComponent {
   //   price: new FormControl(0, [] // validadores sincronos, [] // validadores asincronos),
   //   inStorage: new FormControl(0),
   // })
+
+  isValidField(field: string): boolean {
+    return !!this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `El campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
+        case 'min':
+          return `El valor debe ser mayor a ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset({
+      name: '',
+      price: 0,
+      inStorage: 0,
+    });
+  }
 }
+
