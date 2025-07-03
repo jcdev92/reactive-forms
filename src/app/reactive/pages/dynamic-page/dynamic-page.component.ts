c7cb14e3e3ce93e8e72ec7a83470c7efbf086eda
jcdev92 <jcdeveloper92@gmail.com>
2025-07-02 20:48:19 -0400

    feat(update): :sparkles: update dynamic page component to handle dyanamic form validation

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  formUtils = FormUtils;
+
+  private fb = inject(FormBuilder);
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', [Validators.required, Validators.minLength(3)]],
+      ['Final Fantasy', [Validators.required, Validators.minLength(3)]],
+    ],
+    Validators.minLength(2) // Validación para que al menos haya 3 juegos
+  )
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+}
