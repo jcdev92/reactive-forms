005343ac5bcd9f6bd1673b14632ace01a3cc6ad8
jcdev92 <jcdeveloper92@gmail.com>
2025-07-03 18:07:48 -0400

    feat(update): :sparkles: add methods for dynamic form arrays, and formcontrol for input

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,11 +1,11 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { FormArray, FormControl, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
   static isValidField(form: FormGroup, field: string): boolean {
     return !!form.controls[field].errors && form.controls[field].touched;
   }
 
-  static getErrorMessage(errors: ValidationErrors): string | null {
+  static getInputErrorMessage(errors: ValidationErrors): string | null {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -19,10 +19,32 @@ export class FormUtils {
     return null;
   }
 
+  static getArrayErrorMessage(errors: ValidationErrors): string | null {
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'minlength':
+          return `El arreglo debe tener al menos ${errors['minlength'].requiredLength} elementos`;
+        case 'required':
+          return 'El arreglo es requerido';
+      }
+    }
+    return null;
+  }
+
   static getFieldError(form: FormGroup, field: string): string | null {
     if (!form.controls[field]) return null;
     const errors = form.controls[field].errors ?? {};
-    return this.getErrorMessage(errors);
+    return this.getInputErrorMessage(errors);
+  }
+
+  static isValidArray(formArray: FormArray): boolean {
+    return !!formArray.errors;
+  }
+
+  static getArrayError(formArray: FormArray): string | null {
+    if (!formArray.errors) return null;
+    const errors = formArray.errors ?? {};
+    return this.getArrayErrorMessage(errors);
   }
 
   static isValidFieldInArray(formArray: FormArray, index: number) {
@@ -35,6 +57,15 @@ export class FormUtils {
   static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
     if (!formArray.controls[index]) return null;
     const errors = formArray.controls[index].errors ?? {};
-    return this.getErrorMessage(errors);
+    return this.getInputErrorMessage(errors);
+  }
+
+  static isValidInput(input: FormControl): boolean {
+    return !!input.errors && input.touched;
+  }
+
+  static getInputError(input: FormControl): string | null {
+    const errors = input.errors ?? {};
+    return this.getInputErrorMessage(errors);
   }
 }
