35601bcf7d1eb9c428439bfecc1196d77b82b032
jcdev92 <jcdeveloper92@gmail.com>
2025-07-03 18:09:05 -0400

    feat(update): :sparkles: handle input formcontrol, handle form errors, html elements showing input state, etc...

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -17,13 +17,35 @@ export class DynamicPageComponent {
     favoriteGames: this.fb.array([
       ['Metal Gear', [Validators.required, Validators.minLength(3)]],
       ['Final Fantasy', [Validators.required, Validators.minLength(3)]],
-    ],
-    Validators.minLength(2) // Validación para que al menos haya 3 juegos
-  )
+    ], [Validators.minLength(2), Validators.required] // Validaciones del arreglo
+    )
   });
+
+  newGame = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
   get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddGame() {
+    if (this.newGame.invalid) {
+      this.newGame.markAsTouched();
+      return;
+    }
+    this.favoriteGames.push(this.fb.control(this.newGame.value, [Validators.required, Validators.minLength(3)]));
+    this.newGame.reset();
+  }
+
+  onRemoveGame(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log('Form submitted:', this.myForm.value);
   }
 
 }
