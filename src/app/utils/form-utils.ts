7a5d9943fbbb811f42b6597674f5e44cd7fd23b8
jcdev92 <jcdeveloper92@gmail.com>
2025-07-02 20:47:04 -0400

    feat(update): :sparkles: add methods to handle form array controls validation and warning messages

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,14 +1,11 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
   static isValidField(form: FormGroup, field: string): boolean {
     return !!form.controls[field].errors && form.controls[field].touched;
   }
 
-  static getFieldError(form: FormGroup, field: string): string | null {
-    if (!form.controls[field]) return null;
-    const errors = form.controls[field].errors ?? {};
-
+  static getErrorMessage(errors: ValidationErrors): string | null {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -20,5 +17,24 @@ export class FormUtils {
       }
     }
     return null;
+  }
+
+  static getFieldError(form: FormGroup, field: string): string | null {
+    if (!form.controls[field]) return null;
+    const errors = form.controls[field].errors ?? {};
+    return this.getErrorMessage(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors &&
+      formArray.controls[index].touched
+    );
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+    if (!formArray.controls[index]) return null;
+    const errors = formArray.controls[index].errors ?? {};
+    return this.getErrorMessage(errors);
   }
 }
